cda4d944e36bc3ce84c7182f40c27d26cb9ec80f
sameeranihathe <sameeranihathe@gmail.com>
2018-04-14 16:50:28 +0530

    auth module connected to form io and header component created

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormioAuth, FormioAuthRoutes } from 'angular-formio/auth';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormioAuth,
+    RouterModule.forChild(FormioAuthRoutes())
   ],
   declarations: []
 })
